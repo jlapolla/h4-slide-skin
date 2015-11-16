236ba957ce1a47a91cbcdd20397bf7ed0ea67c90
Justin LaPolla <j.a.lapolla@gmail.com>
2015-11-16 04:48:06 +0000

    path-convert.js work in progress
    - Incomplete implementation of path-convert.js.

---
+++ path-convert.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+'use strict';
+
+var md5 = require('md5');
+
+function PathConverter(){};
+var P = {};
+PathConverter.prototype = P;
+P.toDistPath = function(srcPath){
+  var pathArr = srcPath.split('/');
+  var extensionArr = pathArr[pathArr.length - 1].split('.');
+  var hashedPart;
+  var distPath = md5(srcPath);
+  if (this._dirSplits.length){
+    for (var i = 0; i < this._dirSplits.length; i++){
+    }
+  }
+};
